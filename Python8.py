02e07b56e735e09d9f6665ce00ad2ad5562591b7
Diogo Costa <diogo.costa.t0126447@edu.atec.pt>
2025-04-22 14:15:16 +0100

    Python8

---
+++ Python8.py
@@ -0,0 +1,13 @@
+num1 = 10
+print("Série 1:")
+while num1 <= 1000:
+    print(num1, end=", ")
+    num1 += 10
+
+print("\n")  
+
+num2 = 15
+print("Série 2:")
+while num2 <= 995:
+    print(num2, end=", ")
+    num2 += 10
