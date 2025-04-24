e9a9ebe19cc77d84405225fc61c70f74988403c5
DiogoCostaT0126447 <diogo.costa.t0126447@edu.atec.pt>
2025-04-24 08:31:02 +0100

    Update Exercicio1.c

--- c/Exercicio1.c
+++ c/Exercicio1.c
@@ -3,10 +3,12 @@
 // Desenvolva um programa que assuma uma entrada em Segundos e transforme em Horas, Minutos e Segundos.
 
 int main() {
-    int segundos;
-    int horas = segundos / 3600;
+    int segundos = 0;
+    int minutos = 0;
+    
+    horas = segundos / 3600;
     segundos %= 3600;
-    int minutos = segundos / 60;
+    minutos = segundos / 60;
     segundos %= 60;
     
     printf("Insira o tempo em segundos: ");
